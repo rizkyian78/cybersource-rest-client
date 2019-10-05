b981a2e516a734ef65aed797e91cc99fdfd195bb
tooolbox <tooolbox@users.noreply.github.com>
2019-10-04 21:21:22 -0700

    Removed some unneeded files, tweaked generation script.

--- generator/cybersource_go_sdk_gen.sh
+++ generator/cybersource_go_sdk_gen.sh
@@ -9,18 +9,4 @@ rm -rf ../models
 
 echo Command to generate SDK
 
-swagger generate client -A CyberSource -f cybersource-rest-spec.json -t ../
-
-# echo Batch script for changing accept type
-# powershell -Command "(Get-Content ..\src\Api\SearchTransactionsApi.js) | ForEach-Object { $_ -replace 'accepts = \[''application/json;charset=utf-8', 'accepts = [''*/*'} | Set-Content ..\src\Api\SearchTransactionsApi.js"
-
-# REM powershell -Command "(Get-Content ..\src\index.js) | ForEach-Object { $_ -replace \"require\('./api/Download([DTXS]{3})Api'\), \", \"\" } | Set-Content ..\src\index.js"
-
-# REM powershell -Command "(Get-Content ..\src\index.js) | ForEach-Object { $_ -replace \"'api/Download([DTXS]{3})Api', \", \"\" } | Set-Content ..\src\index.js"
-
-# REM powershell -Command "(Get-Content ..\src\index.js) | ForEach-Object { $_ -replace \"Download([DTXS]{3})Api, \", \"\" } | Set-Content ..\src\index.js"
-
-
-# git checkout ..\README.md
-
-# git checkout ..\package.json
+swagger generate client -A Cybersource -f cybersource-rest-spec.json -t ../
